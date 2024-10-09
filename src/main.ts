70f7c32f583a87d7674b8d8ad66839d3d4f5bfe7
Charlize <extrabutterychar@gmail.com>
2024-10-09 02:14:14 -0700

    Finished step 2 - clicking increases a counter

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,27 @@ header.innerHTML = gameName;
 app.append(header);
 
 // create a button
+const counter = document.createElement("div");
 const button = document.createElement("button");
+let count: number = 0;
 button.innerHTML = "🍨";
-button.onclick = () => {
-  // event occur
-  console.log("is clicked");
-};
+counter.innerHTML = `${count} calories`; 
+
+// button.onclick = () => {
+//   // event occur
+//   console.log("is clicked");
+//   count += 1;
+//   console.log('count: ', count);
+// };
+
+button.addEventListener("click", countIncrease);
+
+function countIncrease() {
+    console.log("is clicked");
+    count += 1;
+    console.log('count: ', count);
+    counter.innerHTML = `${count} calories`; 
+}
 
 app.append(button);
+app.append(counter);
