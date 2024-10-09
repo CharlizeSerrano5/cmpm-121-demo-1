99ae13b2e03dd1ed821a49c7b5d1fe4f0b831353
Charlize <extrabutterychar@gmail.com>
2024-10-09 01:52:58 -0700

    Step 1 commit - creating a button to click

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,13 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// create a button
+const button = document.createElement('button');
+button.innerHTML = "🍨";
+button.onclick = () => {
+    // event occur
+    console.log('is clicked');
+};
+
+app.append(button);
