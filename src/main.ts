d183ff6793a43d93be2c58cc40390681bcd5f872
Charlize <extrabutterychar@gmail.com>
2024-10-09 01:54:39 -0700

    Committing step 1 - button created

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ header.innerHTML = gameName;
 app.append(header);
 
 // create a button
-const button = document.createElement('button');
+const button = document.createElement("button");
 button.innerHTML = "🍨";
 button.onclick = () => {
-    // event occur
-    console.log('is clicked');
+  // event occur
+  console.log("is clicked");
 };
 
 app.append(button);
