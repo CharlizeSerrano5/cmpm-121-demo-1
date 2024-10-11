2ea9688a9f100d03893a8d9191c24051bd99fe2d
Charlize <extrabutterychar@gmail.com>
2024-10-11 02:45:21 -0700

    Completed Step 4 - Continuous Growth

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,39 @@ function countIncrease() {
   counter.innerHTML = `${count} calories`;
 }
 
-setInterval(countIncrease, 1000);
+function setCount(increase: number) {
+    console.log("is set");
+    count = increase; // it's being rewritten
+    console.log("count: ", count);
+    counter.innerHTML = `${count} calories`;
+  }
+// setInterval(countIncrease, 1000);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
+// I took reference from requestAnimationFrame() for the elapse function.
+// The example function I used was function animate() where they used performance.now()
+// Now I am checking if the value has reached 1 to indicate 1000 ms has passed.
+// when it passes then the count increases
+const zero = performance.now();
+let compare = 0;
+// let temp = 0;
+requestAnimationFrame(elapse);
+function elapse(timestamp: number) {
+    const value = (timestamp - zero) / 1000;
+    // the moment the value turns into the next value 1->2
+    const check = value - (value % 1);
+    // check becomes a number without a remainder
+    if (check > compare) {
+        // when the value is greater than the original compare value
+        // set compare and then increase the count
+        compare = check;
+        countIncrease();
+        requestAnimationFrame((t) => elapse(t)); 
+        // recall the function and wait till check finally becomes a new value
+    } else {
+        requestAnimationFrame((t) => elapse(t)); 
+    }
+}
 
 app.append(button);
 app.append(counter);
