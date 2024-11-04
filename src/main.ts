5e0e3f14673f1eee0fce1a1f2d8b03edf63f25db
Charlize <extrabutterychar@gmail.com>
2024-11-04 06:43:57 -0800

    Addition to D1.c - Separation of display logic and item logic

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,6 @@ class Item {
   cost: number;
   rate: number;
   amount: number;
-  button: HTMLButtonElement;
-  display: HTMLElement;
-  content: HTMLElement;
   desc: string;
 
   constructor(
@@ -25,34 +22,21 @@ class Item {
     rate: number,
     amount: number,
     desc: string,
-    // button: HTMLButtonElement,
-    // display: HTMLElement,
   ) {
     this.name = name;
     this.cost = cost;
     this.rate = rate;
     this.amount = amount; // amount will be called
-    this.button = document.createElement("button");
-    this.display = document.createElement("div");
-    this.content = document.createElement("div");
     this.desc = desc;
-    this.content.innerHTML = this.desc;
   }
   increaseAmount() {
     // increase the amount purchased of this item
     this.amount++;
-    this.updateAmount();
   }
 
   increaseCost() {
     // exponentially grow the cost
     this.cost = this.cost * 1.15;
-    this.updateButton();
-  }
-
-  updateButton() {
-    const costNum = this.cost.toFixed(2);
-    this.button.innerHTML = `Purchase ${this.name} for ${costNum}`;
   }
 
   purchaseUpgrade() {
@@ -62,9 +46,7 @@ class Item {
     updateDisplay();
     requestAnimationFrame(elapse);
   }
-  updateAmount() {
-    this.display.innerHTML = `${this.amount} Gallons of ${this.name} Milk has been purchased.`;
-  }
+
   updateItem() {
     // increase cost + amount existing of item
     this.increaseAmount();
@@ -81,48 +63,75 @@ class Item {
 const counter = document.createElement("div");
 const button = document.createElement("button");
 const growthDisplay = document.createElement("div");
+app.append(growthDisplay);
+app.append(button);
+app.append(counter);
 
 const availableItems: Item[] = [
   new Item("Vanilla", 10, 0.1, 0, "Pure, plain, simple vanilla."),
   new Item("Strawberry", 100, 2.0, 0, "A little bit more refreshing flavor."),
   new Item("Banana", 250, 15, 0, "Makes the monkeys go faster."),
-  new Item(
-    "Chocolate",
-    1000,
-    50,
-    0,
-    "A classic, probably don't give to the monkeys.",
-  ),
-  new Item(
-    "Coffee",
-    2500,
-    200,
-    0,
-    "Bout to go fast with this flavor town delight.",
-  ),
+  new Item("Chocolate",1000,50,0,"A classic, probably don't give to the monkeys.",),
+  new Item("Coffee",2500, 200, 0, "Bout to go fast with this flavor town delight.",),
 ];
 
 let totalCount: number = 0;
 let totalIncrease: number = 0;
 button.innerHTML = "🍨";
 button.addEventListener("click", clickIncrease);
-
+const buttons: HTMLButtonElement[] = [];
+const displays: HTMLElement[] = [];
 availableItems.map((item) => {
-  item.button.disabled = true;
-  item.button.addEventListener("click", () => {
+  // https://chat.brace.tools/c/38fd3aec-616c-4736-9858-c7d59d586df8
+  // I referenced a lot of Brace recommendations for cleaning up divergent code
+  // I now separate the button logic and store the displays inside of arrays
+  // I used most of the recommendations that Brace offered but I changed where the logic is being used
+  // I also did not use some of the functions it offered since they were unneccessary
+  const button = document.createElement("button");
+  button.disabled = true;
+  const display = document.createElement("div");
+  const content = document.createElement("div");
+  
+  button.classList.add("button");
+  display.classList.add("display");
+  content.classList.add("content");
+
+  buttons.push(button);
+  displays.push(display);
+
+  updateItemDisplay(item, button, display);
+  content.innerHTML = item.desc;
+
+  app.append(button);
+  app.append(content);
+  app.append(display);
+  button.addEventListener("click", () => {
     item.purchaseUpgrade();
+    updateItemDisplay(item, button, display);
   });
 });
 
-function checkPurchase() {
-  for (const item of availableItems) {
-    if (totalCount >= item.cost) {
-      item.button.disabled = false;
-    } else {
-      item.button.disabled = true;
-    }
+function updateItemDisplay(item: Item, button: HTMLButtonElement, display: HTMLElement) {
+    const costNum = item.cost.toFixed(2);
+    button.innerHTML = `Purchase ${item.name} for ${costNum}`;
+    display.innerHTML = `${item.amount} Gallons of ${item.name} Milk has been purchased.`;
+    checkPurchase(item, button);
+}
+
+function checkAllPurchases(items: Item[], buttons: HTMLButtonElement[]) {
+  for (let i = 0; i < items.length; i++) {
+    checkPurchase(items[i], buttons[i]);
   }
 }
+
+function checkPurchase(item: Item, button: HTMLButtonElement) {
+    if (totalCount >= item.cost) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+}
+
 
 function updateDisplay() {
   const increaseNum = totalIncrease.toFixed(1);
@@ -132,7 +141,7 @@ function updateDisplay() {
 function updateCounter() {
   const countNum = totalCount.toFixed(1);
   counter.innerHTML = `${countNum} tea spoons`;
-  checkPurchase();
+  checkAllPurchases(availableItems, buttons);
 }
 
 // on click purchase will deduct
@@ -167,15 +176,4 @@ function increasePerSecond(newTime: number) {
   }
 }
 
-for (const item of availableItems) {
-  item.updateButton();
-}
 updateDisplay();
-app.append(growthDisplay);
-app.append(button);
-app.append(counter);
-for (const item of availableItems) {
-  app.append(item.button);
-  app.append(item.desc);
-  app.append(item.display);
-}
